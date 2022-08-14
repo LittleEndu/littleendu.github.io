5672c69ee0c6a63b29411ca3a1af0a4105c95b7e
Endrik Tombak <littleendu@gmail.com>
2022-08-14 15:23:04 +0300

    Test template script

---
+++ template/template.manager.user.js
@@ -0,0 +1,183 @@
+// ==UserScript==
+// @name         Template Manager
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Main script that manages the templates for other scripts
+// @author       LittleEndu
+// @grant        none
+// ==/UserScript==
+
+function currentTime() {
+    return Date.now() / 1000;
+}
+
+class Template {
+    constructor(source, priority, loaderMountPoint, templateMountPoint, x, y, frameWidth, frameHeight, frameCount = null, frameRate = null, startTime = null) {
+        // save params
+        this.source = source;
+        this.priority = priority;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.frameCount = frameCount;
+        this.frameRate = frameRate;
+        this.startTime = startTime;
+        this.lastFrame = -1;
+
+        // create element to hold the image
+        this.imageLoader = document.createElement('img');
+        this.imageLoader.onload = () => {
+            this.frameWidth = this.frameWidth || this.imageLoader.naturalWidth;
+            this.frameHeight = this.frameHeight || this.imageLoader.naturalHeight;
+            this.atlasSize = Math.round(this.imageLoader.naturalWidth / this.frameWidth);
+        }
+        this.imageLoader.style = `
+            position: absolute; 
+            top: 0; 
+            left: 0; 
+            width: 1px; 
+            height: 1px; 
+            opacity: ${Number.MIN_VALUE}; 
+            pointer-events: none;
+        `;
+        this.imageLoader.crossOrigin = 'Anonymous';
+        this.imageLoader.src = source;
+        loaderMountPoint.appendChild(this.imageLoader) // firefox would otherwise not load the image
+
+        // create element to hold the template
+        this.templateElement = document.createElement('img');
+        this.templateElement.style = `
+            position: absolute;
+            top: ${y}px;
+            left: ${x}px;
+            width: ${frameWidth}px;
+            height: ${frameHeight}px;
+            pointer-events: none;
+            image-rendering: pixelated;
+        `;
+        this.templateElement.priority = priority;
+        // mount template element to the template mount point according to the priority
+        // get all template elements and sort them by priority
+        let templateElements = templateMountPoint.children;
+        let templateElementsArray = Array.from(templateElements);
+        // remove from array any element where priority is undefined
+        templateElementsArray = templateElementsArray.filter(element => element.priority !== undefined);
+        // if there are no elements with priority, just append the template element to the template mount point
+        if (templateElementsArray.length === 0) {
+            templateMountPoint.appendChild(this.templateElement);
+        } else {
+            // add the new template element to the array
+            templateElementsArray.push(this.templateElement);
+            // sort the array by priority
+            templateElementsArray.sort((a, b) => b.priority - a.priority);
+            // find the index of the new template element in the sorted array
+            let index = templateElementsArray.findIndex(element => element === this.templateElement);
+            // insert the new template element at the index
+            if (index === templateElementsArray.length - 1) {
+                templateMountPoint.appendChild(this.templateElement);
+            } else {
+                templateMountPoint.insertBefore(this.templateElement, templateElementsArray[index + 1]);
+            }
+        }
+    }
+
+    update() {
+        if (this.imageLoader === undefined || !this.imageLoader.complete || this.imageLoader.naturalWidth === 0) {
+            return;
+        }
+
+        let currentFrameIndex = Math.floor((currentTime() - this.startTime) / this.frameRate);
+        let currentFrame = (currentFrameIndex % this.frameCount + this.frameCount) % this.frameCount;
+        if (currentFrame !== this.lastFrame) {
+            this.lastFrame = currentFrame;
+            let scaledCanvas = document.createElement('canvas');
+            scaledCanvas.width = this.frameWidth;
+            scaledCanvas.height = this.frameHeight;
+            let scaledContext = scaledCanvas.getContext('2d');
+            if (this.frameCount !== null || this.frameWidth === this.imageLoader.naturalWidth && this.frameHeight === this.imageLoader.naturalHeight) {
+                // animated template or 1:1 template (so nothing to scale)
+                let frameX = currentFrame % this.atlasSize;
+                let frameY = Math.floor(currentFrame / this.atlasSize);
+
+                // draw the frame
+                scaledContext.drawImage(
+                    this.imageLoader,
+                    frameX * this.frameWidth, frameY * this.frameHeight,
+                    this.frameWidth, this.frameHeight,
+                    0, 0,
+                    this.frameWidth, this.frameHeight
+                )
+            } else {
+                // non-animated template that needs to be scaled
+                scaledContext.drawImage(
+                    this.imageLoader,
+                    0, 0,
+                    this.imageLoader.naturalWidth, this.imageLoader.naturalHeight,
+                    0, 0,
+                    this.frameWidth, this.frameHeight
+                )
+            }
+            // our website should help putting an already scaled image into an atlas so
+            // animated templates that need to be scaled can not exist
+
+            // dither the image in scaledCanvas
+            let data = scaledContext.getImageData(0, 0, this.frameWidth, this.frameHeight)
+            let ditheredData = new ImageData(this.frameWidth * 3, this.frameHeight * 3)
+            for (let y = 0; y < this.frameHeight; y++) {
+                for (let x = 0; x < this.frameWidth; x++) {
+                    let index = (y * this.frameWidth + x) * 4
+                    let middlePixelIndex = ((y * 3 + 1) * ditheredData.width + x * 3 + 1) * 4;
+                    ditheredData.data[middlePixelIndex] = data.data[index];
+                    ditheredData.data[middlePixelIndex + 1] = data.data[index + 1];
+                    ditheredData.data[middlePixelIndex + 2] = data.data[index + 2];
+                    ditheredData.data[middlePixelIndex + 3] = data.data[index + 3];
+                }
+            }
+
+            // convert to data url and set the image
+            let ditheredCanvas = document.createElement('canvas')
+            ditheredCanvas.width = ditheredData.width
+            ditheredCanvas.height = ditheredData.height
+            let ditheredContext = ditheredCanvas.getContext('2d')
+            ditheredContext.putImageData(ditheredData, 0, 0)
+            this.templateElement.src = ditheredCanvas.toDataURL()
+
+            console.log(`updated ${this.source} to frame ${currentFrame}/${this.frameCount}`)
+        }
+    }
+}
+
+async function initTemplatesFromJsonUrl(url, loaderMountPoint, templateMountPoint, priority = 0, depth = 0, alreadyLoaded = []) {
+    let rv = [];
+    if (depth > 10) {
+        return rv
+    }
+    let _url = new URL(url);
+    if (alreadyLoaded.includes(`${_url.origin}${_url.pathname}`)) {
+        return rv
+    }
+    alreadyLoaded.push(`${_url.origin}${_url.pathname}`)
+    // do some cache busting
+    _url.searchParams.append((Math.random() + 1).toString(36).substring(7), Date.now().toString(36));
+    let response = await fetch(_url);
+    let json = await response.json();
+    // load our templates
+    for (let template of json.templates) {
+        let t = new Template(
+            template.url,
+            priority++,
+            loaderMountPoint,
+            templateMountPoint,
+            template.x, template.y,
+            template.frameWidth, template.frameHeight,
+            template.frameCount,
+            template.frameRate,
+            template.startTime
+        );
+        rv.push(t);
+    }
+    // load templates from other json files
+    for (let child of json.children) {
+        rv = rv.concat(await initTemplatesFromJsonUrl(child.url, loaderMountPoint, templateMountPoint, priority, depth + 1));
+    }
+    return rv;
+}
