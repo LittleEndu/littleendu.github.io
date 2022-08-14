92bca81e99312ac5ecd56a37fa3f8edf5fe5a8df
Endrik Tombak <littleendu@gmail.com>
2022-08-14 20:34:49 +0300

    Do I really need to bump the version for the script to work

--- template/template.manager.user.js
+++ template/template.manager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Template Manager
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Main script that manages the templates for other scripts
 // @author       LittleEndu
 // @grant        GM_xmlhttpRequest
